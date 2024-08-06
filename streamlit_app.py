9b0e821dfc4e2c136f40e5a2bd670a7c3012fd11
ArunRoshan143 <122539338+ArunRoshan143@users.noreply.github.com>
2024-08-06 21:25:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,6 @@ st.info('This app builds a ML model')
 
 with st.expander('data'):
   st.info('This app builds a ML model')
-        name = st.text_input('Name')
-        age = st.number_input('Age', min_value=1, max_value=100)
-        symptoms = st.selectbox('Select your primary symptom', options=unique_symptoms)
-        sleep_cycle = st.selectbox('What is your sleep cycle?', ['4 hours', '6 hours', '8 hours'])
-        activity_level = st.selectbox('How is your life activity level?', ['active', 'very active', 'less active', 'lazy'])
-        medical_history = st.selectbox('Do you have a medical history of any of the following diseases?',
-                                               ["Diabetes", "Hypertension", "Asthma", "Heart Disease", "Cancer", "Arthritis", "Thyroid Disorder", "None of the above"])
-        submitted = st.form_submit_button("Submit")
+  submitted = st.form_submit_button("Submit")
   
 
