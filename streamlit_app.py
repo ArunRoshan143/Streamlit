faa84b6ebad25147aabb079be51297f40eaab293
ArunRoshan143 <122539338+ArunRoshan143@users.noreply.github.com>
2024-08-07 07:44:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,13 @@ st.info('This app builds a ML model')
 
 with st.expander('data'):
   st.info('This app builds a ML model')
-  submitted = st.form_submit_button("Submit")
+  # submitted = st.form_submit_button("Submit")
+stm.set_page_config(
+    page_title="My Streamlit App",
+    page_icon=":smiley:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
   
 
